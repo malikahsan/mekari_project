c47cc4479cd55991b851581c837059a23252d7dd
Ahsan Malik <sann.lix@gmail.com>
2019-07-01 20:45:49 +0700

    initial commit from local

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,10 @@
 class Company < ApplicationRecord
-  has_many :employees
+  has_many :employees, dependent: :destroy
+  has_many :policies, dependent: :destroy
 
   validates :name, presence: true
+
+  def company_name
+    return "#{name.titleize}"
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,8 +2,9 @@ class Employee < ApplicationRecord
   belongs_to :company
   has_and_belongs_to_many :policies
 
-  validates :name, presence: true
-  validates :email, uniqueness: true
+  validates :name, format: {with: /\A[a-zA-Z\s]{2,50}\z/i}, presence: true
+  validates :email, format: { with: /\A[a-zA-Z0-9.\/_{|}-]+@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, uniqueness: true
+  validates :phone, numericality: true, length: { minimum: 10, maximum: 13 }, uniqueness: true, presence: true
 
   acts_as_nested_set
 end
